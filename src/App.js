048edd9ae60b1c996cd959ce6f21a602f5149dff
wassim93 <wassim.benhassen@esprit.tn>
2022-01-04 14:12:51 +0000

    fixing logo issue and adding hooks with network call

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import Header from "./components/Header";
+import axios from "axios";
 
 const App = () => {
-  return <div className="App">Hello</div>;
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters`
+      );
+      //console.log(result.data);
+      setItems(result.data);
+      setIsLoading(false);
+    };
+    fetchItems();
+  }, []);
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
 };
 
 export default App;
